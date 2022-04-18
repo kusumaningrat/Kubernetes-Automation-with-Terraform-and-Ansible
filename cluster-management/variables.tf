73a7f5a1c4f04df933e47625bc6b7e15e09115f4
Kusuma16 <kusumanetcom@gmail.com>
2022-04-18 16:47:25 +0800

    Adding some script

--- cluster-management/variables.tf
+++ cluster-management/variables.tf
@@ -1,6 +1,6 @@
 variable "ssh_key" {
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwtyc3e8BwM3PYb32hWQGG2fddzfmn0EJvzEROFOg7xwogsnWtESjE5nfr+zcZTbw47j9zpdg7N/KUfv/y1CIlkbaudeveo3gRF2AfTQZ0WQUve0YAFgQ/eLv+CqlL+1e0WCDtjBfAfjC8iAF4ijkVWe2LGNXQh28YHmXoaPxGMd5VPcnNgg2uW4XAZp8M0UdThmS/CMBQA6c6WGobtfHnkErVf3/zXvn9X/Kw52Eja4re+6WxLQYtNkoZ8DYOoz1seGXlCzlbouVB/ym9Q0NIt+3101WPzL1IH8vKcmbWUQlhN+Ij0kXiNKusVs7uLn8mLx2XuH9qMcpVH78W+LxmhkFYq8meZ9ZFz659VmckFhW6Lew5VQwnTN6tfV8mBN4OwrB+DIh9tQQpg7YvPMIHDL9ruq/W9qVwt7IKSyapoLOxcNRE2WHfWjkWdT+qjzv4n7vJyA7/w6kU/wFpQPnlAtKKAPvvDGOFlcmy+Y54qwxvg0nw8NsSN+2jUUeqbLE= kusumaningrat@kusumaningrat"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJuS4tf9FUDqhnpn/rPM+NtUVW6YypDj7nLF3qshSiw5KThMbH5Wcx+PrSx0lD92MkZTozTiz0zCEJfyIqUCQxuoabbIsT+D/+EZMsAxQUyHj1ZFhhuymX4s2fgR9CjJ8F3UsiODlC/Nn8lui40jQBjF1TG083hAC0+bFr63ZRawsVseP94ZbFhii9fswG8OvR1OfZ2zo7JLG4jfdDJx8y/H8AjJMX2+0FWOkC4bN5ZTbxN5w6ZWstxsyvL7RB8sUvTwR9IdZKAwbGsvo2ydwhq+Nmw2sYYH8aYafDlMKy6It9dQ/WUuF9QrR5sYCShe2uYiy15SUvo7EWQiAQqnmr kusumaningrat@core-server"
   description = "SSH Key"
 }
 
